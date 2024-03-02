e18a053a1e14f85ffa74adc5637b61ec2b4f45ba
MaksMusic <Maksmusic05@yandex.ru>
2024-03-02 14:47:26 +0300

    add ApplicationScope for get count connected server

--- src/main/java/ru/maksmusic/mvc_authorization/controllers/LoginController.java
+++ src/main/java/ru/maksmusic/mvc_authorization/controllers/LoginController.java
@@ -1,10 +1,7 @@
 package ru.maksmusic.mvc_authorization.controllers;
 
-
-import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
